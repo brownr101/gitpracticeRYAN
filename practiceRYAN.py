edd6fc22f5584f8b6da047de72a0ea2061ac60dd
RB <rb@RBMACHINElocal.local>
2020-03-03 15:01:17 -0800

    yeetus mcyeezy

--- practiceRYAN.py
+++ practiceRYAN.py
@@ -1,5 +1,5 @@
 # Partner 1: Ryan Brown
-# Partner 2: Alden McVay
+# Partner 2: Alden Cheeseball
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
